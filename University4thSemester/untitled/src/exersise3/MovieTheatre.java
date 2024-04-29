927f8a5cc6b3c65faa53eccda148c82b263d4206
Yane Yanev <93374652+Yaneww11@users.noreply.github.com>
2024-04-29 22:47:48 +0300

    Update MovieTheatre.java

--- University4thSemester/untitled/src/exersise3/MovieTheatre.java
+++ University4thSemester/untitled/src/exersise3/MovieTheatre.java
@@ -4,8 +4,11 @@ public class MovieTheatre extends TradeCenter{
     protected double price_for_ticket;
     protected int count_ticket;
 
-    public MovieTheatre(String name, String address, double profit, double price_for_ticket, int count_ticket) {
-        super(name, address, profit);
+    public MovieTheatre() {
+    }
+
+    public MovieTheatre(String name, String address, double price_for_ticket, int count_ticket) {
+        super(name, address);
         this.price_for_ticket = price_for_ticket;
         this.count_ticket = count_ticket;
     }
@@ -15,9 +18,6 @@ public class MovieTheatre extends TradeCenter{
         return "MovieTheatre{" +
                 "price_for_ticket=" + price_for_ticket +
                 ", count_ticket=" + count_ticket +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", profit=" + profit +
                 "} " + super.toString();
     }
 }
